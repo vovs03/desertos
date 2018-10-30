767ed58871646a2e874ba4158a0528de73b8bbdf
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2018-10-30 09:14:15 +0300

    Add: Install :atom: NodeJS into LM19

---
+++ scripts/environment/nodejs_desertos_env_lm19.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#################### ::: NodeJS ::: ##############################################3
+
+# 2018-10-30
+# 09:03
+# Author: Vladimir Pavlychev <vladimir.keyjoo@gmail.com> https://github.com/KeyJoo/desertos
+
+#/bin/bash --login
+
+clear
+
+### https://www.digitalocean.com/community/tutorials/node-js-ubuntu-16-04-ru
+
+echo ""
+echo "\e[44m >>> Now 'NodeJS' installing on this PC "
+echo "\e[0m" # Normal text
+
+# Install nodejs 18 Mb
+
+sudo apt-get install nodejs -y
+
+###
