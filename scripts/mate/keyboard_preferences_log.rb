5c3ab8438c6e16bf7ed079b648aa225191105be4
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2019-03-08 11:58:20 +0300

    Add: :eight::tulip: Install MATE for PC/OS :penguin: LM19 OsArt-011

---
+++ scripts/mate/keyboard_preferences_log.rb
@@ -0,0 +1,12 @@
+# === LOG ===
+# 2019
+# GUI ENG LM 19. 
+# Какая-то магия, изменил 1 галочку, но потом отменил этот чекбокс 
+# в Preferences > Keyboard Preferences > Accessibility 
+# [x] Accessibility features can be toggled with keyboard shortcuts
+# 
+
+puts 'Магия с настройкой переключения клавиатуры при помощи <Shift> + <Alt>'
+puts '2019-03-08 11:30 MSK'
+puts 'Установлена графическая среда Linux Mint 19 MATE'
+puts 'PC/OS: OsArt-011'
---
+++ scripts/mate/mate-install.sh
@@ -0,0 +1,10 @@
+# !/usr/bin/bash
+
+# ~/code/github/desertos/scripts/mate/mate-install.sh
+
+# 2019-03-08
+# 11:53
+# Author: Vladimir Pavlychev <vladimir.keyjoo@gmail.com> https://github.com/KeyJoo/desertos
+
+# Install LM 19 MATE
+sudo apt install mint-meta-mate -y
