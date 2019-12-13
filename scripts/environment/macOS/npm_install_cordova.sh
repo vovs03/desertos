1447ed57f6f80a0fb2f1897ed809ac0b32e7260f
Vladimir Pavlychev <vladimir.keyjoo@gmail.com>
2019-12-13 19:02:05 +0300

    Add: :recycle: 9.0.0 (cordova-lib@9.0.1) #66

---
+++ scripts/environment/macOS/npm_install_cordova.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# 2019-12-13
+
+echo "Cordova installing now!"
+sleep 2
+
+sudo npm install -g cordova                                                                                                              ‹ruby-2.6.3›
+
+:<< T
+Password:
+/usr/local/bin/cordova -> /usr/local/lib/node_modules/cordova/bin/cordova
++ cordova@9.0.0
+added 456 packages from 363 contributors in 16.907s
+T
+
+cordova --version 
