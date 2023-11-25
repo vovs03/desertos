ca9b9cbab2e822d67f0aa54fc1bbd560951d0774
Vladimir Victor Pavlychev <vladimir.keyjoo@gmail.com>
2023-11-25 18:19:55 +0300

    Add: ffmpeg

---
+++ osx/brew/ffmpeg.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+# 2023-11-25 18:12
+# Vladimir Victor Pavlychev
+# Ventura 13.2.1
+# https://github.com/vovs03/desertos
+# /Users/husky/Projects/desertos/osx/brew/ffmpeg.sh
+# 🍏 UUID:: A1660404-5706-42A8-AD43-2189DF3C9F6C
+
+clear
+
+#### Set before run:
+#### chmod +x ./nickel.sh
+echo "Preparing for install 🔋 ffmpeg"
+
+#### https://nickel-lang.org/getting-started
+sleep 0.85
+brew install ffmpeg
