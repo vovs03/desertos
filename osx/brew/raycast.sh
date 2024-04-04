efcbef53514592b1d265e214263b8bebf0166dce
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2024-04-04 16:58:35 +0300

    Add: raycast, OSX app like alfred

---
+++ osx/brew/raycast.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+# 2024-04-04 16:30
+# Vladimir Victor Pavlychev
+# Ventura 13.6.4
+# /Users/$USER/Projects/desertos
+
+clear
+
+#### Set before run:
+#### chmod +x ./tmux.sh
+echo "Preparing for install raycast"
+sleep 0.75
+brew install --cask raycast
+
+#### https://www.raycast.com/teams
