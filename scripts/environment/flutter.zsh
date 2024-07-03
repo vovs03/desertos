90208e036b988b409e3ea78d872b87c77ea30dc5
Vladimir Pavlychev <vladimir.keyjoo@gmail.com>
2024-07-03 15:28:52 +0300

    Fix: :rocket: get flutter(by url)

--- scripts/environment/flutter.zsh
+++ scripts/environment/flutter.zsh
@@ -17,7 +17,7 @@ sleep 0.85
 
 # wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.2-stable.tar.xz
 FILE="flutter_linux_3.22.2-stable.tar.xz"
-# URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/$FILE"
+URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/$FILE"
 # _FPATH="/Users/vovs03/Projects/desertos/scripts/environment"
 
 # https://ru.stackoverflow.com/questions/141466/Проверка-существования-файла
